59e7a1a920000ecf21b7b57695c90e7ab97f057b
Muhamad Biki Hamzah <biki.hamzah@gmail.com>
2024-02-12 20:10:32 +0700

    move template salary

--- Domain/Entities/HR/TemplateSalary.cs
+++ Domain/Entities/HR/TemplateSalary.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace Domain.Entities.Master;
+namespace Domain.Entities.HR;
 
 [Table("template_salary", Schema = "hr")]
 public partial class TemplateSalary
