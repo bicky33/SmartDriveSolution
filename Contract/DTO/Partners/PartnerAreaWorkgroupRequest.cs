0d71c0487e895dbd66819eef268269a31a08ac99
Muhamad Biki Hamzah <biki.hamzah@gmail.com>
2024-02-13 23:21:44 +0700

    ADD init dto

---
+++ Contract/DTO/Partners/PartnerAreaWorkgroupRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract.DTO.Partners
+{
+    public record PartnerAreaWorkgroupRequest(
+        int PawoPatrEntityid,
+        string PawoArwgCode,
+        int PawoUserEntityid,
+        string? PawoStatus
+    );
+}
---
+++ Contract/DTO/Partners/PartnerRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract.DTO.Partners
+{
+    public record PartnerRequest(
+        int PartEntityid,
+        string? PartName,
+        string? PartAddress,
+        DateTime? PartJoinDate,
+        string? PartAccountNo,
+        string? PartNpwp,
+        string? PartStatus,
+        int PartCityId
+    );
+}
